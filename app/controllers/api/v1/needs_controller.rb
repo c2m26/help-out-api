394000f88128b325fbb3a0ec4c777ce3ca418228
c2m26 <carlos@c2m-webdev.com>
2020-01-30 23:08:33 +0100

    need api controller method index working fine

--- app/controllers/api/v1/needs_controller.rb
+++ app/controllers/api/v1/needs_controller.rb
@@ -1,22 +1,25 @@
 class Api::V1::NeedsController < ApplicationController
         
     def create
-        need = Need.new(need_params)
-        if need.save
-            render json: {
-              data: need,
-              status: :ok
-            } 
-              
-            
-        else
-            render json: {
-              errors: need.errors,
-              status: :unprocessably_entity
-            }
-              
-            
-        end
+      need = Need.new(need_params)
+      if need.save
+        render json: {
+          data: need,
+          status: :ok
+        } 
+                  
+      else
+        render json: {
+          errors: need.errors,
+          status: :unprocessably_entity
+        }
+      end
+    end
+
+    def index
+      needs = Need.all
+      render json: needs
+      
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :registrations, only: [:create]
       delete :logout, to: "sessions#logout"
       get :logged_in, to: "sessions#logged_in"
-      resources :needs, only: [:create]
+      resources :needs, only: [:create, :index]
     end
   end
   
