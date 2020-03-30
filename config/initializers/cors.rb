c55d005cb54e1fd4cde977bd7e7df4a624b5aaf1
c2m26 <carlos@c2m-webdev.com>
2020-03-30 10:26:07 +0200

    cors credentials true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         origins 'https://help-out-oc-p8.herokuapp.com/'
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-credentials: false
+credentials: true
     end
 end
