3f596c474f3e341be06885081c134803a4ca7a0b
c2m26 <carlos@c2m-webdev.com>
2020-03-30 10:37:47 +0200

    added action dispatch default header in appl.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module HelpOutApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://help-out-api-oc-p8.herokuapp.com/',
+      'Access-Control-Request-Method' => %w{GET POST PATCH OPTIONS}.join(",")
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
